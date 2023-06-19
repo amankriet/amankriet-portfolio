3f1510a21732fc2a729f4e431818235fcaf843d4
amankriet <amankriet@gmail.com>
2023-06-19 13:19:48 +0530

    fixing collapsing navbar issue

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Collapse, Dropdown, initTE } from "tw-elements";
+
+initTE({ Collapse, Dropdown });
 
 ReactDOM.render(
   <React.StrictMode>
